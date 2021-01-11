28fad485ebaadca06ccfdb01adaf0284fe39d3cf
zcason <zuri.cason@gmail.com>
2021-01-11 11:47:06 -0600

    added connection string

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,5 +7,8 @@ module.exports = {
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "password": process.env.MIGRATION_DB_PASS,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
