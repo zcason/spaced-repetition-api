015fe48177169ed4b4259ad6ed33e5ada2fa8266
zcason <zuri.cason@gmail.com>
2021-01-11 11:37:06 -0600

    change db urls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://dunder_mifflin@localhost/spaced-repetition',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
+    || "postgresql://dunder_mifflin@localhost/spaced-repetition-test",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
